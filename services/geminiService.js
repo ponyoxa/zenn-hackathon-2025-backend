26ac4e3210e0a1dabe166878362b2bceb0852664
mae616 <oya2.mi@hotmail.co.jp>
2025-02-08 22:00:10 +0900

    Refine Gemini AI response parsing and prompt generation logic

--- services/geminiService.js
+++ services/geminiService.js
@@ -15,19 +15,38 @@ class GeminiService {
         });
 
         // 生成リクエストの実行
-        const result = await model.generateContent(prompt);
+        let resultJson = "";
 
-        const response = result.response.candidates[0].content.parts[0].text;
+        for (let i = 0; i < 3; i++) {
+            const result = await model.generateContent(prompt);
 
-        try {
-            const content = response.substring(
-                response.indexOf("{"),
-                response.lastIndexOf("}") + 1
-            );
-            return JSON.parse(content);
-        } catch (e) {
-            throw new Error(`AIからのレスポンスの解析に失敗しました: ${e}`);
+            const response =
+                result.response.candidates[0].content.parts[0].text;
+
+            try {
+                const content = response.substring(
+                    response.indexOf("{"),
+                    response.lastIndexOf("}") + 1
+                );
+                resultJson = JSON.parse(content);
+            } catch (e) {
+                continue;
+            }
+            if (resultJson) {
+                resultJson.level_metrics = resultJson.level_metrics.filter(
+                    (metric) =>
+                        !metric.label.includes("テックスキル") &&
+                        !metric.label.includes("継続性") &&
+                        !metric.label.includes("課題解決") &&
+                        !metric.label.includes("コミュニケーション") &&
+                        !metric.label.includes("独自性") &&
+                        !metric.label.includes("問題解決")
+                );
+                return resultJson;
+            }
         }
+        throw new Error("AIからのレスポンスの解析に失敗しました");
+
     }
 }
 
