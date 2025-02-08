e59beb5493ca5be9f0fb67ff25b0d09f6db938f1
mae616 <oya2.mi@hotmail.co.jp>
2025-02-09 00:35:08 +0900

    Update CORS and response headers for Gemini API endpoint
    - Add explicit Content-Type header for JSON responses
    - Update .gcloudignore to exclude .secret file

--- index.js
+++ index.js
@@ -42,6 +42,7 @@ app.post("/ask-gemini", async (req, res) => {
         res.set("Access-Control-Allow-Origin", "*");
         res.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
         res.set("Access-Control-Allow-Headers", "Content-Type");
+        res.set("Content-Type", "application/json");
 
         res.json(response);
     } catch (error) {
